ab264b2270e62d2beb110b8065dd58bf5ac49146
Rene Richter <Richterrettich@gmail.com>
2015-09-03 17:21:31 +0200

    Initial commit

---
+++ src/main/java/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAutoConfiguration
+public class DiscoveryServiceApplication {
+  public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
